fb8eccbafec0204029e88ca8c633706a660b710c
agrippa1994 <agrippa1994@icloud.com>
2019-11-09 00:23:42 +0100

    cache graphql responses in LocalStorage

--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -2,13 +2,22 @@ import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import {persistCache} from 'apollo-cache-persist';
 
 // const uri = 'https://ytdl.mani94.space/graphql'; // <-- add the URL of the GraphQL server here
 const uri = '/graphql'; // <-- add the URL of the GraphQL server here
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache: cache,
+  storage: window.localStorage,
+});
+
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
+    cache: cache,
   };
 }
 
